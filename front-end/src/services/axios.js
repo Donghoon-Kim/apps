476fe071589ddbff7cc20d328ef036e717627914
kimehdgns <kimehdgns@ohjic.com>
2018-07-15 10:31:19 +0900

    #5 JWT 관련 이슈
    - 이미 로드되어 있느 ㄴ리엑트 컴포넌트의 토큰 접근에 대한 문제

--- front-end/src/services/axios.js
+++ front-end/src/services/axios.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 const domain = 'http://localhost:8080';
 const contextPath = '';
 const domainContextPath = domain + contextPath;
-const headerConfig = {'authorization': localStorage.getItem('bookapp_token')};
+const headerConfigLoader = () => {
+    return {'authorization': localStorage.getItem('bookapp_token')};
+};
 
 export function getSearchApi() {
-    return axios.get(domainContextPath + '/searchApis', {headers: headerConfig});
+    return axios.get(domainContextPath + '/searchApis', {headers: headerConfigLoader()});
 }
 
 export function getBooks(query, page, target, searchApiIdx) {
@@ -16,7 +18,7 @@ export function getBooks(query, page, target, searchApiIdx) {
             page: page,
             target: target,
         },
-        headers: headerConfig
+        headers: headerConfigLoader()
     });
 }
 
@@ -24,22 +26,22 @@ export function saveQueryHistory(query) {
     return axios.post(domainContextPath + '/queryHistories', {
         query: query,
     },{
-        headers: headerConfig
+        headers: headerConfigLoader()
     });
 }
 
 export function getQueryhistory() {
-    return axios.get(domainContextPath + '/queryHistories', {headers: headerConfig});
+    return axios.get(domainContextPath + '/queryHistories', {headers: headerConfigLoader()});
 }
 
 export function saveBook(book, searchApiIdx) {
-    return axios.post(domainContextPath + '/searchApis/'+searchApiIdx+'/books', book, {headers: headerConfig});
+    return axios.post(domainContextPath + '/searchApis/'+searchApiIdx+'/books', book, {headers: headerConfigLoader()});
 }
 
 export function removeBook(bookIdx, searchApiIdx) {
-    return axios.delete(domainContextPath + '/searchApis/'+searchApiIdx+'/books/'+bookIdx, {headers: headerConfig});
+    return axios.delete(domainContextPath + '/searchApis/'+searchApiIdx+'/books/'+bookIdx, {headers: headerConfigLoader()});
 }
 
 export function saveBookmark(bookmark){
-    return axios.post(domainContextPath + '/bookmarks', bookmark, {headers: headerConfig});
+    return axios.post(domainContextPath + '/bookmarks', bookmark, {headers: headerConfigLoader()});
 }
